aad2034d97dbac98de8670f93b2d5a80e1861302
Faav <52789876+withdrew@users.noreply.github.com>
2021-04-22 11:48:47 -0400

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -6,18 +6,18 @@
 	      }else{
 	         darkMode();
 	      }
-	      function darkMode() {
+	      function lightMode() {
 	      document.body.className = "lightmode-background";
 	         document.getElementById("darkmodebtn").className = "lightmode-button";
-	         document.getElementById("darkmodebtn").onclick = lightMode;
-	         localStorage.setItem('mode', 'dark');
+	         document.getElementById("darkmodebtn").onclick = darkMode;
+	         localStorage.setItem('mode', 'light');
 	         mode = localStorage.getItem('mode');
 	      }
 
-	      function lightMode() {
+	      function darkMode() {
 	      document.body.className = "darkmode-background";
 	         document.getElementById("darkmodebtn").className = "darkmode-button";
-	         document.getElementById("darkmodebtn").onclick = darkMode;
-	         localStorage.setItem('mode', 'light');
+	         document.getElementById("darkmodebtn").onclick = lightMode;
+	         localStorage.setItem('mode', 'dark');
 	         mode = localStorage.getItem('mode');
 	      }
